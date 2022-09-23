9b899660ae6283b8dfd34831574b699f40faa801
MariuszMalinka <malinkamariusz@gmail.com>
2022-09-24 00:17:10 +0200

    The code benefited from some clarifying comments.

--- estimation.py
+++ estimation.py
@@ -1,8 +1,7 @@
 import numpy as np
-#x=np.array([[1,2],[4,3],[9,8]]) # x to macierz składająca się z warunków atmosferycznych
-#y=np.array([[3],[6],[10]]) # y to wektor składający się z idealnych wartości nastawienia
 
 def estimation(x,y):
-    # b = (X^T X)^(-1) X^T Y # b to wektor współczynników stojących przy wartościach w równaniu wyznaczającym idealną wartość nastawienia
+    ## b is the array of coefficients standing by the values in the equation determining the optimal output value 
+    ## Mathematical notation b = (X^T X)^(-1) X^T Y 
     b=np.matmul(np.matmul(np.linalg.inv(np.matmul(x.T,x)),x.T),y)
     return b
--- obliczenia.py
+++ obliczenia.py
@@ -1,3 +1,4 @@
 def obliczenia (b):
+    # deleted in further version
     o = b[0]*float(input('Termometr: '))+b[1]*float(input('Grubość lodu: '))+b[2]*float(input('Ciśnienie atmosferyczne: '))+b[3]*float(input('Wilgotność powietrza: '))+b[4]*float(input('Zachmurzenie: '))
     return o
