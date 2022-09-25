95ac9834fa324084eea7c832addf5e732746bd41
MariuszMalinka <malinkamariusz@gmail.com>
2022-09-25 21:09:52 +0200

    First release is here!

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ICE
 
-Multidimensional model calculating optimal output value.
+ICE is a software creating multidimensional model to calculate optimal output value.
+
+Quick usage guide:
+1. Place ICE.exe in the same folder as Excel file with data.
+2. Run ICE and follow the instructions in command prompt.
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from array import array
+import time
 import importuj
 import estimation
 
@@ -9,10 +9,11 @@ import estimation
 b = estimation.estimation(x,y)
 
 ## User enters values to calculate optimal value (4 lines)
-print('Please enter input values to calculate optimal output value.')
+print('\nPlease enter input values to calculate optimal output value.')
 w=list()
 for i in range(len(k)):
     w.append(float(input(str(k[i]) + ': ')))
+    time.sleep(0.1)
 
 ## Linear equation consisting of array from estimation and values entered by user (3 lines)
 z=0
@@ -21,3 +22,4 @@ for i in range(len(k)):
 
 ## Results presentation to user
 print ('The optimal value is: ' + str(round(z[0],1)))
+time.sleep(5)
