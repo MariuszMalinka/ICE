39cbdcbd3d44fbc1bc82922cb9f9a18abfba9f08
MariuszMalinka <malinkamariusz@gmail.com>
2022-09-25 18:05:28 +0200

    The importuj function now allows to set boundaries for imported data.

--- importuj.py
+++ importuj.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import pandas as pd
 def importuj ():
@@ -9,12 +10,25 @@ def importuj ():
     except:
         a = pd.read_excel(str(user_input))
 
-    ## Setting the boundaries of data to operate on (5 lines)
-    wierszstart = 0#int(input('Wprowadź index wiersza początkowego (domyślnie 0): '))
-    wierszkoniec = -1#int(input('Wprowadź index wiersza końcowego (dla całego zbioru wprowadź "-1"): '))
-    optymalna = 2#int(input('Wprowadź index kolumny zawierającej wartość optymalną (2): '))
-    wejsc_start = 3#int(input('Wprowadź index pierwszej kolumny zawierającej wartości zewnętrzne (3): '))
-    wejsc_koniec = 8#int(input('Wprowadź index ostatniej kolumny zawierającej wartości zewnętzrzne (8): '))
+    ## User states the import settings option (18 lines)
+    print('Choose between importing option.\n Write "Standard" if your file is compatible with standard file format.\n Write "Manual" if you want to set data boundaries by yourself.\nFor additional information check the user manual.')
+    impsetting = input()
+    if impsetting == 'Standard' or impsetting == 'standard':
+        wierszstart = 0
+        wierszkoniec = -1
+        optymalna = 2
+        wejsc_start = 3
+        wejsc_koniec = 8
+    elif impsetting == 'Manual' or impsetting == 'manual':
+        wierszstart = int(input('Please enter index of the first row: '))
+        wierszkoniec = int(input('Please enter index of the last row: '))
+        optymalna = int(input('Please enter index of the column with optimal values: '))
+        wejsc_start = int(input('Please enter index of the first column with input values: '))
+        wejsc_koniec = int(input('Please enter index of the last column with input values: '))
+    else:
+        print('Something went wrong, maybe you misspelled the option.')
+        time.sleep(5)
+        quit()
 
     ## Creating arrays for further operations (3 lines)
     k = a.columns[wejsc_start:wejsc_koniec]
