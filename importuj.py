36d13989efc2920a61000c9b583ddcc867a3f055
MariuszMalinka <malinkamariusz@gmail.com>
2022-09-29 23:31:43 +0200

    File format recognized correctly

--- importuj.py
+++ importuj.py
@@ -1,9 +1,10 @@
+import os
 import time
 import numpy as np
 import pandas as pd
 def importuj ():
     
-    ## Import and distinguish a file (14 lines)
+    ## Import a file (24 lines)
     user_input = input('Please enter file name or path: ')
 
     if user_input.find ('.xlsx') >= 0 :
@@ -14,13 +15,24 @@ def importuj ():
         a = pd.read_csv(user_input)
         return importujCSV(a)
 
+    ## Distinguish file extension (17 lines)
     else:
-        try:
+        b=np.array(os.listdir())
+        for i in range(len(b)):
+            d=b[i].split(".")
+            if d[0] == 'Data':
+                j=d[1]
+                break
+        if j == 'xlsx' :
             a = pd.read_excel(user_input + str('.xlsx'))
             return importujExcel(a)
-        except:
+        elif j == 'csv' :
             a = pd.read_csv(user_input + str('.csv'))
             return importujCSV(a)
+        else:
+            print('Sorry, we could not find such file.')
+            time.sleep(5)
+            quit()
 
 def importujExcel (a):
     print('Choose between importing option.\n Write "Standard" if your file is compatible with standard file format.\n Write "Manual" if you want to set data boundaries by yourself.\nFor additional information check the user manual.')
@@ -32,7 +44,7 @@ def importujExcel (a):
         wierszkoniec = -1
         optymalna = 2
         wejsc_start = 3
-        wejsc_koniec = -1
+        wejsc_koniec = 8
     
     ## User sets import boundaries (19 lines)
     elif impsetting == 'Manual' or impsetting == 'manual':
@@ -62,7 +74,7 @@ def importujExcel (a):
 
     ## Creating arrays for further operations (3 lines)
     k = a.columns[wejsc_start:wejsc_koniec]
-    x = np.array(a.iloc[wierszstart:wierszkoniec,wejsc_start:wejsc_koniec+1])
+    x = np.array(a.iloc[wierszstart:wierszkoniec,wejsc_start:wejsc_koniec])
     y = np.array(a.iloc[wierszstart:wierszkoniec,optymalna:optymalna+1])
     return (x,y,k)
 
