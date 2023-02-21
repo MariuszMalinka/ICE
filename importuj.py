a28475357c1fc1b180f6ffb4308804338074989c
MariuszMalinka <malinkamariusz@gmail.com>
2023-02-21 23:39:11 +0100

    Support for data from CSV added

--- importuj.py
+++ importuj.py
@@ -2,43 +2,60 @@ import os
 import time
 import numpy as np
 import pandas as pd
+import csv
 def importuj ():
     
-    ## Import a file (24 lines)
+    class Data:
+        def __init__(self,type,optimal_output,input_variables,column_names,table):
+            self.type = type
+            self.optimal_output = optimal_output
+            self.input_variables = input_variables
+            self.column_names = column_names
+            self.table = table
+    
+    data1=Data(0,0,0,0,0)
+
+
+    ## Import a file
     user_input = input('Please enter file name or path: ')
 
     if user_input.find ('.xlsx') >= 0 :
-        a = pd.read_excel(user_input)
-        return importujExcel(a)
+        data1.table = pd.read_excel(user_input)
+        data1.type = 'Excel'
+        return importujExcel(data1)
 
     elif user_input.find ('.csv') >= 0 :
-        a = pd.read_csv(user_input)
-        return importujCSV(a)
+        data1.table = pd.read_csv(user_input)
+        data1.type = 'CSV'
+        return importujCSV(data1)
 
-    ## Distinguish file extension (17 lines)
+    ## Distinguish file extension
     else:
         b=np.array(os.listdir())
+        j=0
         for i in range(len(b)):
             d=b[i].split(".")
-            if d[0] == 'Data':
+            if d[0] == user_input:
                 j=d[1]
                 break
         if j == 'xlsx' :
-            a = pd.read_excel(user_input + str('.xlsx'))
-            return importujExcel(a)
+            data1.table = pd.read_excel(user_input + str('.xlsx'))
+            data1.type = 'Excel'
+            return importujExcel(data1)
         elif j == 'csv' :
-            a = pd.read_csv(user_input + str('.csv'))
-            return importujCSV(a)
+            data1.table = pd.read_csv(user_input + str('.csv'), header=None)
+            data1.type = 'CSV'
+            return importujCSV(data1)
         else:
             print('Sorry, we could not find such file.')
             time.sleep(5)
             quit()
 
-def importujExcel (a):
+def importujExcel (data1):
     print('Choose between importing option.\n Write "Standard" if your file is compatible with standard file format.\n Write "Manual" if you want to set data boundaries by yourself.\nFor additional information check the user manual.')
     impsetting = input()
 
-    ## Setting import boundaries according to standard excel file (6 lines)
+    ## Setting import boundaries according to standard excel file
     if impsetting in ['Standard','standard']:
         wierszstart = 0
         wierszkoniec = -1
@@ -46,9 +63,9 @@ def importujExcel (a):
         wejsc_start = 3
         wejsc_koniec = 8
     
-    ## User sets import boundaries (21 lines)
+    ## User sets import boundaries
     elif impsetting in ['Manual','manual']:
-        g = list(a.columns)
+        g = list(data1.table.columns)
         wierszstart = int(input('Please enter index of the first row: '))
         wierszkoniec = int(input('Please enter index of the last row: '))
 
@@ -77,13 +94,17 @@ def importujExcel (a):
         time.sleep(5)
         quit()
 
-    ## Creating arrays for further operations (3 lines)
-    k = a.columns[wejsc_start:wejsc_koniec]
-    x = np.array(a.iloc[wierszstart:wierszkoniec,wejsc_start:wejsc_koniec])
-    y = np.array(a.iloc[wierszstart:wierszkoniec,optymalna:optymalna+1])
-    return (x,y,k)
+    ## Creating arrays for further operations
+    data1.column_names = data1.table.columns[wejsc_start:wejsc_koniec]
+    data1.input_variables = np.array(data1.table.iloc[wierszstart:wierszkoniec,wejsc_start:wejsc_koniec])
+    data1.optimal_output = np.array(data1.table.iloc[wierszstart:wierszkoniec,optymalna:optymalna+1])
+    print(data1.input_variables)
+    print(data1.optimal_output)
+    return (data1)
 
-def importujCSV (a):
-    print('CSV support coming soon!')
-    time.sleep(5)
-    quit()
+def importujCSV (data1):
+    data1.optimal_output = np.array(data1.table.iloc[:,0:1])
+    data1.input_variables = np.array(data1.table.iloc[:,1:])
+    print(data1.input_variables)
+    print(data1.optimal_output)
+    return (data1)
--- main.py
+++ main.py
@@ -2,24 +2,44 @@ import time
 import importuj
 import estimation
 
-## Importing arrays for further use in calculation (x,y) and values names (k)
-(x,y,k) = importuj.importuj()
+## Importing variable for further use
+(data1) = importuj.importuj()
 
 ## Calculating the coefficients standing by the values in the further equation
-b = estimation.estimation(x,y)
+b = estimation.estimation(data1.input_variables,data1.optimal_output)
 
-## User enters values to calculate optimal value (4 lines)
-print('\nPlease enter input values to calculate optimal output value.')
-w=list()
-for i in range(len(k)):
-    w.append(float(input(str(k[i]) + ': ')))
-    time.sleep(0.1)
+if data1.type == 'Excel':
+    ## User enters values to calculate optimal value
+    print(b)
+    print('\nPlease enter input values to calculate optimal output value.')
+    w=list()
+    for i in range(len(data1.column_names)):
+        w.append(float(input(str(data1.column_names[i]) + ': ')))
+        time.sleep(0.1)
 
-## Linear equation consisting of array from estimation and values entered by user (3 lines)
-z=0
-for i in range(len(k)):
-    z=z+b[i]*w[i]
+    ## Linear equation consisting of array from estimation and values entered by user
+    z=0
+    for i in range(len(data1.column_names)):
+        z=z+b[i]*w[i]
+    
+    ## Results presentation to user
+    print ('The optimal value is: ' + str(round(z[0],1)))
+    time.sleep(5)
 
-## Results presentation to user
-print ('The optimal value is: ' + str(round(z[0],1)))
-time.sleep(5)
+if data1.type == 'CSV':
+    ## User enters values to calculate optimal value
+    print(b)
+    print('\nPlease enter input values to calculate optimal output value.')
+    w=list()
+    for i in range(data1.input_variables.shape[1]):
+        w.append(float(input('Variable ' + str(i+1) + ': ')))
+        time.sleep(0.1)
+    
+    ## Linear equation consisting of array from estimation and values entered by user
+    z=0
+    for i in range(data1.input_variables.shape[1]):
+        z=z+b[i]*w[i]
+    
+    # Results presentation to user
+    print ('The optimal value is: ' + str(round(z[0],1)))
+    time.sleep(5)
