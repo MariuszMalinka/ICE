215f4cd0a828f7dffb75ac78abcce279e3c5a6da
MariuszMalinka <malinkamariusz@gmail.com>
2023-03-12 20:07:13 +0100

    Estimation option change

--- main.py
+++ main.py
@@ -5,10 +5,25 @@ import estimation
 ## Importing variable for further use
 (data1) = importuj.importuj()
 
-## Calculating the coefficients standing by the values in the further equation
-b = estimation.estimation(data1.input_variables,data1.optimal_output)
-
 if data1.type == 'Excel':
+
+    ## User selects estimation options
+    print('Choose between estimation options.\n Write "Hour based" to treat data based on corresponding hour. \n Write "Day based" to treat data based on corresponding day of the week.\n Write "All" to treat all data. \nFor additional information check the user manual.')
+    
+    estimation_option = input()
+
+    if estimation_option in ['Hour based','hour based']:
+        print('Function being built. All data will be taken into consideration during estimation.')
+    elif estimation_option in ['Day based','day based']:
+        print('Function being built. All data will be taken into consideration during estimation.')
+    elif estimation_option in ['All','all']:
+        print('All data will be taken into consideration during estimation.')
+    else:
+        print('Something went wrong, maybe you misspelled the option. All data will be taken into consideration during estimation.')
+        
+    ## Calculating the coefficients standing by the values in the further equation
+    b = estimation.estimation(data1.input_variables,data1.optimal_output)
+
     ## User enters values to calculate optimal value
     print('\nPlease enter input values to calculate optimal output value.')
     w=list()
@@ -26,6 +41,10 @@ if data1.type == 'Excel':
     time.sleep(5)
 
 if data1.type == 'CSV':
+
+    ## Calculating the coefficients standing by the values in the further equation
+    b = estimation.estimation(data1.input_variables,data1.optimal_output)
+
     ## User enters values to calculate optimal value
     print('\nPlease enter input values to calculate optimal output value.')
     w=list()
