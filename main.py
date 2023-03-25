d1da507c6fb014cad70f14e467349d12d1ad178a
MariuszMalinka <malinkamariusz@gmail.com>
2023-03-25 18:23:28 +0100

    Small changes and comments

--- main.py
+++ main.py
@@ -5,76 +5,89 @@ import pandas as pd
 from datetime import datetime
 
 ## Importing variable for further use
-(data1) = importuj.importuj()
+(imported_data) = importuj.importuj()
 
-if data1.type == 'Excel':
+if imported_data.type == 'Excel':
 
-    ## User selects estimation options
+    ## This section prompts the user to select an estimation option and modifies the input data based on the option chosen. Prompt the user to choose an estimation option.
     print('Choose between estimation options.\n Write "Hour based" to treat data based on corresponding hour. \n Write "Day based" to treat data based on corresponding day of the week.\n Write "All" to treat all data. \nFor additional information check the user manual.')
     
+    ## Get the user's estimation option input.
     estimation_option = input()
     
+    ## If the user selected "Hour based", prompt for the hour to use for estimation and modify the input data accordingly.
     if estimation_option in ['Hour based','hour based']:
+        ## Prompt the user for the hour to use for estimation.
         hour_check_str = input('Insert the time to which the relevant data should be treated. \n')
         hour_check = datetime.strptime(hour_check_str, '%H:%M:%S').time()
-        data1.table.iloc[:, 1] = pd.to_datetime(data1.table.iloc[:, 1], format='%H:%M:%S')
-        matching_rows = data1.table.iloc[:, 1].apply(lambda x: x.strftime('%H:%M:%S')) == hour_check_str
+        ## Convert the second column of the imported_data table to datetime format using the given format.
+        imported_data.table.iloc[:, 1] = pd.to_datetime(imported_data.table.iloc[:, 1], format='%H:%M:%S')
+        ## Check for rows that match the user input.
+        matching_rows = imported_data.table.iloc[:, 1].apply(lambda x: x.strftime('%H:%M:%S')) == hour_check_str
+        ## If there are matching rows, modify the input variables and optimal output variables accordingly.
         if matching_rows.any():
-            data1.input_variables = data1.input_variables[matching_rows]
-            data1.optimal_output = data1.optimal_output[matching_rows]
+            imported_data.input_variables = imported_data.input_variables[matching_rows]
+            imported_data.optimal_output = imported_data.optimal_output[matching_rows]
         else:
-            pass
-
+            pass ## Do nothing if there are no matching rows.
+    
+    ## If the user selected "Day based", prompt for the day to use for estimation and modify the input data accordingly.
     elif estimation_option in ['Day based','day based']:
         day_check = input('Insert the day to which the relevant data should be treated. \n')
-        matching_rows = data1.table.iloc[:, 0] == day_check
-        matching_rows = matching_rows[:-1]
+        ## Extract rows that match the user's choice of day
+        matching_rows = imported_data.table.iloc[:, 0] == day_check
+        ## Check if any rows match the user's choice
         if matching_rows.any():
-            data1.input_variables = data1.input_variables[matching_rows]
-            data1.optimal_output = data1.optimal_output[matching_rows]
+            ## If any matching rows are found, select the corresponding input and output data
+            imported_data.input_variables = imported_data.input_variables[matching_rows]
+            imported_data.optimal_output = imported_data.optimal_output[matching_rows]
         else:
-            pass
+            pass ## Do nothing if there are no matching rows.
+    
+    ## If the user selected "All", no modification is necessary.
     elif estimation_option in ['All','all']:
         print('All data will be taken into consideration during estimation.')
+    
+    ## If the user input an unrecognized option, assume "All" was intended.
     else:
         print('Something went wrong, maybe you misspelled the option. All data will be taken into consideration during estimation.')
         
     ## Calculating the coefficients standing by the values in the further equation
-    b = estimation.estimation(data1.input_variables,data1.optimal_output)
+    coefficients = estimation.estimation(imported_data.input_variables,imported_data.optimal_output)
 
     ## User enters values to calculate optimal value
     print('\nPlease enter input values to calculate optimal output value.')
-    w=list()
-    for i in range(len(data1.column_names)):
-        w.append(float(input(str(data1.column_names[i]) + ': ')))
+    entered_values=list()
+    for i in range(len(imported_data.column_names)):
+        entered_values.append(float(input(str(imported_data.column_names[i]) + ': ')))
         time.sleep(0.1)
 
     ## Linear equation consisting of array from estimation and values entered by user
-    z=0
-    for i in range(len(data1.column_names)):
-        z=z+b[i]*w[i]
+    optimal_output=0
+    for i in range(len(imported_data.column_names)):
+        optimal_output=optimal_output+coefficients[i]*entered_values[i]
     
     ## Results presentation to user
-    print ('The optimal value is: ' + str(round(z[0],1)))
+    print ('The optimal value is: ' + str(round(optimal_output[0],1)))
     time.sleep(5)
 
-if data1.type == 'CSV':
+if imported_data.type == 'CSV':
 
     ## Calculating the coefficients standing by the values in the further equation
-    b = estimation.estimation(data1.input_variables,data1.optimal_output)
+    coefficients = estimation.estimation(imported_data.input_variables,imported_data.optimal_output)
 
     ## User enters values to calculate optimal value
     print('\nPlease enter input values to calculate optimal output value.')
-    w=list()
-    for i in range(data1.input_variables.shape[1]):
-        w.append(float(input('Variable ' + str(i+1) + ': ')))
+    entered_values=list()
+    for i in range(imported_data.input_variables.shape[1]):
+        entered_values.append(float(input('Variable ' + str(i+1) + ': ')))
         time.sleep(0.1)
     
     ## Linear equation consisting of array from estimation and values entered by user
-    z=0
-    for i in range(data1.input_variables.shape[1]):
-        z=z+b[i]*w[i]
+    optimal_output=0
+    for i in range(imported_data.input_variables.shape[1]):
+        optimal_output=optimal_output+coefficients[i]*entered_values[i]
     
-    # Results presentation to user
-    print ('The optimal value is: ' + str(round(z[0],1)))
+    ## Results presentation to user
+    print ('The optimal value is: ' + str(round(optimal_output[0],1)))
     time.sleep(5)
